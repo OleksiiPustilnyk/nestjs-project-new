165744cd564cbcb517abda6e2ebc077d838ccfe9
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2024-01-10 02:12:21 +0200

    added prisma+mongoDb, create resource  products and users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UsersController } from './users/users.controller'
+import { UsersModule } from './users/users.module'
+import { ProductsModule } from './products/products.module'
+import { DatabaseModule } from './database/database.module';
+import { Database } from './database/database';
+import { DatabaseService } from './database/database.service';
+import { DatabaseResolver } from './database/database.resolver';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UsersModule, ProductsModule, DatabaseModule],
+    controllers: [AppController, UsersController],
+    providers: [AppService, Database, DatabaseService, DatabaseResolver],
 })
 export class AppModule {}
