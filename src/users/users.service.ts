3afbb3ca4dcd3d902e40db8fa236872785959bd0
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2024-01-11 03:11:05 +0200

    added zod for users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { DatabaseService } from 'src/database/database.service'
@@ -8,34 +8,58 @@ export class UsersService {
     constructor(private databaseService: DatabaseService) {}
 
     create(createUserDto: CreateUserDto) {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { id, ...data } = createUserDto
         return this.databaseService.user.create({
             data: {
-                ...data,
+                ...createUserDto,
                 address: {
-                    create: [],
+                    createMany: {
+                        data: createUserDto.address,
+                    },
                 },
             },
         })
     }
 
     findOne(id: string) {
-        return this.databaseService.user.findUnique({ where: { id } })
+        return this.databaseService.user.findUnique({
+            where: { id },
+            include: { address: true },
+        })
     }
 
-    update(id: string, updateUserDto: Partial<UpdateUserDto>) {
-        // eslint-disable-next-line
+    update(id: string, updateUserDto: UpdateUserDto) {
+        const existingEntry = this.findOne(id)
+        if (!existingEntry) {
+            throw new NotFoundException()
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const { address, ...data } = updateUserDto
-        return this.databaseService.user.update({
-            where: { id },
-            data: {
-                ...data,
-            },
+        return this.databaseService.$transaction(async tx => {
+            const pendingUpdates: Promise<any>[] = []
+            for (const a of address || []) {
+                // noinspection TypeScriptValidateJSTypes
+                pendingUpdates.push(
+                    tx.address.update({ where: { id: a.id }, data: a }),
+                )
+            }
+
+            // Updating all addresses
+            await Promise.all(pendingUpdates)
+
+            // Updating user in the end
+            // noinspection TypeScriptValidateJSTypes
+            return tx.user.update({
+                where: { id },
+                data,
+            })
         })
     }
 
     remove(id: string) {
+        const existingEntry = this.findOne(id)
+        if (!existingEntry) {
+            throw new NotFoundException()
+        }
         return this.databaseService.user.delete({ where: { id } })
     }
 }
